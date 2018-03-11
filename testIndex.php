19686d1a0aa0c819e0c24afcca6cff12ebb5838e
skms95 <saurav.sept11@gmail.com>
2018-03-11 21:45:55 +0530

    Add files via upload

---
+++ testIndex.php
@@ -0,0 +1,170 @@
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" type="text/css" href="/examples/css/bootstrap.min.css">
+<!-- Optional Bootstrap theme -->
+<link rel="stylesheet" href="/examples/css/bootstrap-theme.min.css">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<style>
+.verticalLine {
+  border-left: 1px solid #EFEFF5;
+    height: 5vh;
+}
+.txtNew {
+    word-wrap: break-word;
+    text-align: start;
+    pointer-events: none;
+}
+.fa {
+  padding: 5%;
+  font-size: 30;
+  width: 2%;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+}
+.fa:hover {
+    opacity: 0.7;
+}
+.fa-facebook {
+
+  background: #3B5998;
+  color: white;
+}
+.fa-twitter {
+  background: #55ACEE;
+  color: white;
+}
+.mySlides {display:none;}
+a {
+    text-decoration: none !important;
+}
+.card {
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    width: 40%;
+}
+
+.card:hover {
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.container {
+    padding: 2px 16px ;
+	background-color:#ffffff
+}
+.slideImg {
+  display: block;
+  
+  max-height:500px;;
+  width: auto;
+  height: auto;
+align:center;
+}
+</style>
+<body style="background-color:#E0E2E7;">
+
+<div style="position: relative; width:100%; height: 10vh; margin: 0px auto 0px auto; top: 0px; background-color: rgba(52, 63, 100, 1);">
+
+<img src="/UloomLogo1.png" width="12%"  style="padding-top:0px;height:20vh;">
+
+
+<a  href="/contact.html">
+<span style="color:white;float:right;padding-right:3%;padding-left:0%;padding-top:3vh; font-size:2vh;" class="headerText">CONTACT US</span></a>
+<a href="/about.html">
+<span style="color:white; float:right;left;padding-right:3% ;padding-left:0%;padding-top:3vh;font-size:2vh;" class="headerText">ABOUT US</span></a>
+
+<a  href="/admissions.html">
+<span style="color:white; float:right;left;padding-right:3%;padding-left:0%;padding-top:3vh;font-size:2vh;" class="headerText">ADMISSIONS</span></a>
+
+
+<a href="/testIndex.php">
+<span style="color:white; float:right;left;padding-right:3%;padding-left:0%; padding-top:3vh;font-size:2vh;" class="headerText">HOME</span></a>
+
+</div>
+
+<div class="w3-content w3-section" style="">
+  <img   class="mySlides " src="/slideImages/img1.jpg" style="width:100%;height:70vh;margin-top:0px;align:center">
+  <img class="mySlides " src="/slideImages/img2.jpg" style="width:100%;height:70vh;padding-top:0px;align:center">
+  <img class="mySlides " src="/slideImages/img3.jpg" style="width:100%;height:70vh;padding-top:0px;align:center">
+  <img   class="mySlides " src="/slideImages/img4.jpg" style="width:100%;height:70vh;margin-top:0px;align:center">
+  <img class="mySlides " src="/slideImages/img5.jpg" style="width:100%;height:70vh;padding-top:0px;align:center">
+  <img class="mySlides " src="/slideImages/img6.jpg" style="width:100%;height:70vh;padding-top:0px;align:center">
+  <img   class="mySlides " src="/slideImages/img7.jpg" style="width:100%;height:70vh;margin-top:0px;align:center">
+  <img class="mySlides " src="/slideImages/img8.jpg" style="width:100%;height:70vh;padding-top:0px;align:center">
+  <img class="mySlides " src="/slideImages/img9.jpg" style="width:100%;height:70vh;padding-top:0px;align:center">
+  <img class="mySlides " src="/slideImages/img10.jpg" style="width:100%;height:70vh;padding-top:0px;align:center">
+  
+</div>
+</div>
+<div style="border:1px; solid black;padding-bottom:2%;">
+<div class="card" style="padding-top:10px; height:50vh;width:30%;margin-left:30%;">
+  <img src="/manager.jpg" alt="Avatar" style="width:100%;height:35vh;">
+  
+  <div class="container" >
+    <h4><b style="font-size:3vh;font-size:1.5vw;">Tauqir Hayat Khan</b></h4> 
+    <p style="font-size:2vh">Manager</p> 
+  </div>
+  
+</div>
+</div>
+
+
+<h2 style="overflow: hidden;font-size:4vh;">Mission</h2>
+<p style="overflow: hidden;width:100%;font-size:2.5vh;font-size:1.5vw;">The Mission of Madarsa Shariatul Uloom  is to provide a quality education to children. Our objective is to help the children escape the bonds of poverty and hopelessness by providing education,
+ life skills, values and a caring environment that will empower them to successfully move into the mainstream of society .</p>
+
+
+<div style="width:100%;height:15vh;background-color: rgba(52, 63, 100, 1); padding:top:2vh;">
+
+
+<div style="padding-top:5vh;"></div>
+<span  style="margin-left:15%;margin:right:60%;">
+<a href="https://www.facebook.com/shariatulUloom" target="_blank">
+<img id="comp-imomo2yb0imageimageimage" alt="" data-type="image" src="/fb.png" style="width: 5vh; height: 5vh; object-fit: cover; ">
+</a>
+<a href="https://www.twitter.com/shariatulUloom" target="_blank">
+<img id="comp-imomo2yb1imageimageimage" alt="" data-type="image" src="/twitter.png" style="width: 5vh; height: 5vh; object-fit: cover;">
+<a href="https://www.instagram.com/shariatulUloom" target="_blank">
+
+<img id="comp-imomo2yb2imageimageimage" alt="White Instagram Icon" data-type="image" src="/insta.png" style="width: 5vh; height: 5vh; object-fit: cover;">
+</a>
+<div></div>
+<span style="color:#D2D2DC;font-size:2vh;width:20%;margin-top:20%;">&#9400;Copyright reserved by Shariatul Uloom. &nbsp; Station Road Nai Bazar, Bhadahi (U.P.) 221401</span>
+
+<!--span style="margin-left:50%;padding-top:-15%;">
+<span style="color:white;font-weight:bold;font-size:3vh;margin-top:-10%;">Address</span><br/>
+<span style="color:#D2D2DC;font-size:2vh;padding-left:60%;">Station Road, Bhadohi ,Uttar Pradesh, 221401 </span>
+</span-->
+
+</div>
+</body>
+<script>
+
+	
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 5000); // Change image every 5 seconds
+}
+</script>
+
+
+</html>
